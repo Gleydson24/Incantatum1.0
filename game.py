e7424c8b5d58880c2af9eb6ae9043ad60259115e
Gleydson24 <163334526+Gleydson24@users.noreply.github.com>
2025-08-01 09:36:35 -0300

    Update game.py

--- game.py
+++ game.py
@@ -4,19 +4,21 @@ from core.player import Player
 
 pygame.init()
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1366, 768
+info = pygame.display.Info()
+SCREEN_WIDTH, SCREEN_HEIGHT = info.current_w, info.current_h
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Incantatum - Gameplay")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAP_PATH = os.path.join(BASE_DIR, "assets", "map.png")
 map_img = pygame.image.load(MAP_PATH).convert()
+
 map_bg = pygame.transform.scale(map_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Chão e barreira lateral
-floor_rect = pygame.Rect(5, 729, 1400, 88)
+floor_rect = pygame.Rect(5, 1030, 1400, 88)
 
-player = Player(15, floor_rect.top - 20, floor_rect)
+player = Player(15, floor_rect.top - 20, floor_rect, screen_height=SCREEN_HEIGHT)
 
 clock = pygame.time.Clock()
 FPS = 60
